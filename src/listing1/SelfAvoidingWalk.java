8b74dd7407023c6113482851d5da4e55748d95b7
Elovenko Nikita <livemarker@users.noreply.github.com>
2021-03-18 17:49:38 +0300

    Initial commit

---
+++ src/listing1/SelfAvoidingWalk.java
@@ -0,0 +1,32 @@
+package listing1;
+
+public class SelfAvoidingWalk {
+    public static void main(String[] args) {
+        int n = 50;
+        int trials = 500;
+        int deadEnds = 0;
+        for (int i = 0; i < trials; i++) {
+            boolean[][] a = new boolean[n][n];
+            int x = n / 2, y = n / 2;
+            while (x > 0 && x < n - 1 && y > 0 && y < n - 1) {
+                a[x][y] = true;
+                if (a[x - 1][y] && a[x + 1][y] && a[x][y - 1] && a[x][y + 1]) {
+                    deadEnds++;
+                    break;
+                }
+                double r = Math.random();
+                if (r < 0.25) {
+                    if (!a[x + 1][y]) x++;
+                } else if (r < 0.50) {
+                    if (!a[x - 1][y]) x--;
+                } else if (r < 0.75) {
+                    if (!a[x][y + 1]) y++;
+                } else if (r < 1.00) {
+                    if (!a[x][y - 1]) y--;
+                }
+
+            }
+        }
+        System.out.println(100*deadEnds/trials+"% deadEnds");
+    }
+}
